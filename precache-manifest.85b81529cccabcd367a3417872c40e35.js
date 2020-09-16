6b5eea1c1ca1528702e035d019e6c6ea85a82b43
Priyanshu-e <kushpriyanshu91@gmail.com>
2020-09-17 00:12:04 +0530

    Updates

---
+++ precache-manifest.85b81529cccabcd367a3417872c40e35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6c6df00576d5a2c1d7a6e647447dea",
+    "url": "/FindMind/index.html"
+  },
+  {
+    "revision": "ec95710c6a8efd3c3df6",
+    "url": "/FindMind/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "94cbb8107d5bf9f7b865",
+    "url": "/FindMind/static/css/main.6fb98c2a.chunk.css"
+  },
+  {
+    "revision": "ec95710c6a8efd3c3df6",
+    "url": "/FindMind/static/js/2.b1d10716.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/FindMind/static/js/2.b1d10716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cbb8107d5bf9f7b865",
+    "url": "/FindMind/static/js/main.b0040bc8.chunk.js"
+  },
+  {
+    "revision": "af65cee9ee486bfc80ad",
+    "url": "/FindMind/static/js/runtime-main.432bfea5.js"
+  },
+  {
+    "revision": "be064106f204c78bf7bb3c47234794f6",
+    "url": "/FindMind/static/media/28.be064106.jpg"
+  },
+  {
+    "revision": "50574b35ffced273eca825d0b38f2411",
+    "url": "/FindMind/static/media/img1.50574b35.jpg"
+  },
+  {
+    "revision": "85b86e2f7e5b7031f3b7faad04e4b835",
+    "url": "/FindMind/static/media/img2.85b86e2f.jpg"
+  },
+  {
+    "revision": "4859b3566b4f11b956622421e24a45a6",
+    "url": "/FindMind/static/media/img3.4859b356.jpeg"
+  }
+]);
